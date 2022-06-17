49110163cef14f4e9656b6cd87fb89389c7922c4
Suraj Saroj <sarojsuraj@gmail.com>
2022-06-17 17:02:30 +0530

    completed mail compose

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,8 +1,113 @@
-import './Home.css'
+import "./Home.css";
+import React, { useState } from "react";
+// import ReactQuill from "react-quill";
+// import "../../../node_modules/react-quill/dist/quill.snow.css";
 
-const Home = () =>{
-    return <section className='home-container'>
-        <h1>Welcome to Mail box</h1>
+const Home = () => {
+  const [input, setInput] = useState({
+    email: "",
+    subject: "",
+  });
+  const [message, setMessage] = useState("");
+
+  const changeHandler = (e) => {
+    const { name, value } = e.target;
+    setInput((prevInput) => {
+      return {
+        ...prevInput,
+        [name]: value,
+      };
+    });
+  };
+
+  const messageHandler = (e) => {
+    setMessage(e.target.value);
+  };
+
+  const submitHandler = async (e) => {
+    e.preventDefault();
+
+    try {
+      const res = await fetch(
+        "https://mail-box-1af88-default-rtdb.firebaseio.com/mails.json",
+        {
+          method: "POST",
+          body: JSON.stringify({
+            email: input.email,
+            subject: input.subject,
+            message: message,
+          }),
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+
+      const data = await res.json();
+      if (res.ok) {
+        alert("Mail sent");
+        setInput({
+            email:'',
+            subject:''
+        })
+        setMessage('')
+      } else {
+        let errMsg = "sending mail failed!";
+        if (data && data.error && data.error.message) {
+          errMsg = data.error.message;
+        }
+        throw new Error(errMsg);
+      }
+    } catch (err) {
+      alert(err);
+    }
+  };
+
+  return (
+    <section className="home-container">
+      <h1>Welcome to Mail box</h1>
+      <div className="mail-box">
+        <form onSubmit={submitHandler}>
+          <div className="to">
+            <label>To</label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              value={input.email}
+              onChange={changeHandler}
+            />
+          </div>
+          <div className="to subject">
+            <label>Subject</label>
+            <input
+              type="text"
+              id="subject"
+              name="subject"
+              value={input.subject}
+              onChange={changeHandler}
+            />
+          </div>
+          <div className="subject">
+            {/* <ReactQuill
+           theme="snow"
+           placeholder="write something here..."
+           onChange={messageHandler}
+           value={message}
+           /> */}
+            <textarea
+              value={message}
+              className="messageInput"
+              placeholder="Enter your message here..."
+              onChange={messageHandler}
+            />
+          </div>
+          <button type="submit" className="sendMail">
+            Send ▶
+          </button>
+        </form>
+      </div>
     </section>
-}
-export default Home
+  );
+};
+export default Home;
